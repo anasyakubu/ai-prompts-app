02fbe8169a548931b7227dcf55af0166d43dcf89
anasyakubu <yakubuanas04@gmail.com>
2024-01-13 00:00:29 +0100

    Initial commit

---
+++ app/layout.jsx
@@ -0,0 +1,21 @@
+import "@/styles/globals.css";
+
+export const metadat = {
+  title: "AI Prompts",
+  description: "Discover & Share AI prompts",
+};
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+        <main className="app">{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
