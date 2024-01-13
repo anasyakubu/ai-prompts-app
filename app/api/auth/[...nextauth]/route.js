7de62af8f874ab24c84149c1f666afcb279ee59f
anasyakubu <yakubuanas04@gmail.com>
2024-01-13 22:39:39 +0100

    Added NEXTAUTH env

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
+import User from "@models/user";
+import { connectToDB } from "@utils/database";
+
 // console.log({
 //   clientId: process.env.GOOGLE_ID,
 //   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
@@ -13,11 +16,37 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {},
+  async session({ session }) {
+    const sessionUser = await User.findOne({
+      email: session.user.email,
+    });
+
+    session.user.id = sessionUser._id.toString();
+
+    return session;
+  },
+
   async signIn({ profile }) {
     try {
       // serverless -> lambda -> dynamodb
-    } catch (error) {}
+      await connectToDB();
+      //check if a user is already exits
+      const UserExist = await User.findOne({
+        email: profile.email,
+      });
+      //if not create a new user
+      if (!UserExist) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace(" ", "").toLowerCase(),
+          image: profile.picture,
+        });
+      }
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
   },
 });
 
