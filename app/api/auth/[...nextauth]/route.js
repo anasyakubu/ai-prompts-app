6cd8cee67ca42bfaa656dd1ad976d719dc285012
anasyakubu <yakubuanas04@gmail.com>
2024-01-13 21:22:16 +0100

    Added env Data

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,15 +1,24 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
+// console.log({
+//   clientId: process.env.GOOGLE_ID,
+//   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+// });
+
 const handler = NextAuth({
   providers: [
     GoogleProvider({
-      clientId: "",
-      clientSecret: "",
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
   async session({ session }) {},
-  async signIn({ profile }) {},
+  async signIn({ profile }) {
+    try {
+      // serverless -> lambda -> dynamodb
+    } catch (error) {}
+  },
 });
 
 export { handler as GET, handler as POST };
