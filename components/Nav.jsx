94fb7654ff49dfc6be64507d0bb649d42ad0d958
anasyakubu <yakubuanas04@gmail.com>
2024-01-16 18:08:18 +0100

    Bugs Encounter

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -116,11 +116,11 @@ const Nav = () => {
         ) : (
           <>
             {providers &&
-              Object.values(providers).map((provider) => (
+              Object.values(providers).map((Provider) => (
                 <button
                   type="button"
-                  key={provider.id}
-                  onClick={() => signIn(provider.idp)}
+                  key={Provider.name}
+                  onClick={() => signIn(provider.id)}
                   className="black_btn"
                 >
                   Sign In
