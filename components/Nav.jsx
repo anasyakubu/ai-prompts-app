05450f189dc42aa6694a54cf0097b227174d19b8
anasyakubu <yakubuanas04@gmail.com>
2024-02-01 23:17:25 +0100

    Added toggle

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -3,9 +3,11 @@ import Link from "next/link";
 import Image from "next/image";
 import Logo from "../public/assets/images/ai-logo.svg";
 import User from "../public/assets/images/user-1.jpg";
+import { useState } from "react";
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 const Nav = () => {
+  const [toggleDropdown, setToggleDropdown] = useState(false);
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
