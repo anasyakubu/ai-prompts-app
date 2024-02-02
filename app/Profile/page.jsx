2bf617d86858c5fe33d934e442a8fa61be8ba490
anasyakubu <yakubuanas04@gmail.com>
2024-02-02 16:38:18 +0100

    Get User to Image

--- app/Profile/page.jsx
+++ app/Profile/page.jsx
@@ -1,8 +1,10 @@
 "use client";
 import { useUser } from "@clerk/nextjs";
+import Image from "next/image";
 
 export default function Example() {
   const { isLoaded, isSignedIn, user } = useUser();
+  console.log(user);
 
   if (!isLoaded || !isSignedIn) {
     return null;
@@ -10,7 +12,23 @@ export default function Example() {
 
   return (
     <div>
-      Hello, {user.firstName} {user.lastName} welcome to Clerk
+      Hello, {user.firstName} {user.lastName} {user.username} welcome to Clerk
+      <div className="">
+        {/* <Image
+          src={user.imageUrl}
+          alt={user.username}
+          width={50}
+          height={50}
+          className="rounded-full"
+        /> */}
+        {user.imageUrl && (
+          <img
+            src={user.imageUrl}
+            alt={user.username}
+            className="avatar rounded full"
+          />
+        )}
+      </div>
     </div>
   );
 }
